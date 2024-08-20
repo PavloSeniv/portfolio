14bfe65962acc527f07ab6034a6e71dc6845f86b
pavlo-seniv <p.seniv@skgp.eu>
2024-08-20 21:38:10 +0300

    add skills spoiler

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,8 +2,10 @@ import * as functionsIsWebp from "./modules/_webp_add_class_to_html.js";
 import * as functionHeaderMenuBtnClick from "./modules/headerMenuBtnClick.js";
 
 import {filterBadWords} from "./modules/filterBadWords.js";
+import {spoiler} from "./modules/plugins_spollers_acordeon.js";
 
 functionsIsWebp.isWebp();
 functionHeaderMenuBtnClick.headerMenuBtnClick()
 
-filterBadWords('myForm','message')
+filterBadWords('myForm', 'message')
+spoiler()
