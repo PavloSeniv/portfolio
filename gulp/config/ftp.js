548e4755609ed2ba3ff87ed63ff000fdf2595a7e
pavlo-seniv <p.seniv@skgp.eu>
2024-09-01 18:09:18 +0300

    restored ftp file

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+    host: "127.0.0.1", // Адрес FTP сервера
+    user: "admin", // Ім'я користувача
+    password: "admin", // Пароль
+    parallel: 5, // К-сть одночасних потоків
+};
